96c37a19984db9308d42f4f9202df6982529f18c
xingdongyu <xingdongyu@xiaomi.com>
2018-03-22 10:13:57 +0800

    ok socket

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,74 @@
 window.onload = function() {
-  console.log(111)
+  var mychat = new MyChat()
+  mychat.init()
+}
+//定义一个类用于开发程序
+var MyChat = function() {
+  this.socket = null
+}
+var chattxt = document.getElementById('chat-bottom-input')
+var userlist=document.getElementById('userList')
+/**
+ * 添加方法
+ */
+MyChat.prototype = {
+  init: ()=>{
+    var that = this
+    //连接
+    this.socket = io.connect()
+    //客户端监听连接  成功后
+    this.socket.on('connect',function(){
+      document.getElementById('cover-status').textContent = '请输入昵称'
+      document.getElementById('cover-main').style.display = 'flex'
+      document.getElementById('cover-login-input').focus()
+    })
+    //登录
+    document.getElementById('login-btn').addEventListener('click',function(){
+      var username = document.getElementById('cover-login-input').value
+      //客户端发送数据
+      that.socket.emit('login',username)
+    },false)
+    //登录成功
+    this.socket.on('logiSuccess',function(){
+      document.getElementById('cover-main').style.display = 'none'
+      document.getElementById('chat-bottom-input').focus()
+    })
+    //用户列表更新
+    this.socket.on('userList',function(users) {
+      console.log("脚后跟发的撒",users)
+      userlist.textContent=''
+      users.forEach(function(item){
+        var newuser = document.createElement('li')
+        newuser.textContent = '用户:'+item
+        userlist.appendChild(newuser)
+      })
+    })
+    // 系统消息 包括用户在线人数
+    this.socket.on('system',function(username,usernum,currtype){
+     var msg = username+(currtype === 'login'?'加入':'离开')
+     MyChat.prototype.shownewmsg('系统消息',msg)
+     document.getElementById('chat-top-status').textContent= usernum+ ' 位用户在线'
+    })
+    //发送数据
+    document.getElementById('send-btn').addEventListener('click',function(){
+      var msg = chattxt.value
+      
+      //客户端发送数据
+      that.socket.emit('sendMsg',msg)
+      MyChat.prototype.shownewmsg('我',msg)
+    },false)
+    //消息
+    this.socket.on('newMsg',function(user,msg){
+      console.log("客户端ddd")
+      MyChat.prototype.shownewmsg(user,msg)
+    })
+  },
+  shownewmsg: (user, msg)=> {
+    console.log("gggggggggg")
+    var systemMsg = document.getElementById('chat-top-infoarea')
+    var newMsg  = document.createElement('p')
+    newMsg.innerHTML = user + ' (' + new Date().toTimeString().substr(0, 8) + ') : '+ msg
+    systemMsg.appendChild(newMsg)
+    systemMsg.scrollTop = systemMsg.scrollHeight
+  },
 }
--- server.js
+++ server.js
@@ -3,7 +3,46 @@ var app = express()
 var server = require("http").createServer(app)
 var io = require('socket.io')(server)
 
+var users = []  //当前用户
+
 app.use('/', express.static(__dirname+'/public'))
 server.listen(8998,function(){
   console.log("服务器启动")
+})
+
+
+//socket
+io.on('connection',function(socket){
+  // 监听连接用户
+  socket.on('login',function(username) {
+    console.log("成功建立连接")
+    if(users.indexOf(username) > -1) {
+
+    } else {
+      socket.username = username
+      //给该socket发送数据
+      users.push(username)
+      socket.emit('logiSuccess')
+      //广播 用户 和人数
+      io.sockets.emit('userList',users)
+      io.sockets.emit('system',username, users.length,'login')
+    }
+    
+  })
+  // 监听用户信息
+  socket.on('sendMsg',function(msg){
+    console.log("发送新消息...")
+    //给所有的客户端广播消息
+    socket.broadcast.emit('newMsg',socket.username,msg)
+  })
+  //监听用户离开
+  socket.on('disconnect',function(){
+    console.log("有用户离开",socket.username)
+    if(socket.username !=null) {
+      users.splice(users.indexOf(socket.username),1)
+      //广播
+      socket.broadcast.emit('userList',users)
+     socket.broadcast.emit('system',socket.username,users.length,'logout')
+    }
+  })
 })
